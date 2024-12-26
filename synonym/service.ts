70e1061dc71db23b831398c69c3065a58d3c6ef7
Andrew Wittrock <andy@wittrock.us>
2024-12-26 17:06:07 -0600

    Fix synonym table name

--- synonym/service.ts
+++ synonym/service.ts
@@ -2,5 +2,5 @@ import { ISynonym } from "../../common-shared/synonym/types";
 import { basicCrudService } from "../../core/express/service/common";
 
 export const Synonym = {
-    ...basicCrudService<ISynonym>("synonym", "canonical"),
+    ...basicCrudService<ISynonym>("synonyms", "canonical"),
 }
