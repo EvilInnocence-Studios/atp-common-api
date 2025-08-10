e806d08227c4bb46015125d804123908fb98a15a
Andrew Wittrock <andy@wittrock.us>
2025-08-10 01:06:23 -0500

    Add memoized setting helper

--- setting/service.ts
+++ setting/service.ts
@@ -1,3 +1,4 @@
+import { memoizePromise } from "ts-functional";
 import { ISetting } from "../../common-shared/setting/types";
 import { database } from "../../core/database";
 import { basicCrudService } from "../../core/express/service/common";
@@ -6,4 +7,16 @@ const db = database();
 
 export const Setting = {
     ...basicCrudService<ISetting>("settings", "key"),
+    get: memoizePromise(async (key: string): Promise<string> => {
+        const setting = await db
+            .select("*")
+            .from("settings")
+            .where({ key })
+            .first();
+        if (!setting) {
+            return "";
+        }
+        return setting.value;
+    }, {ttl: 5000}),
 };
+
