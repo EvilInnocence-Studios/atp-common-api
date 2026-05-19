04625d44ead29a7ecc6d2cb8790664a2d9aace26
Andrew Wittrock <andy@wittrock.us>
2026-05-19 13:39:02 -0500

    Add whitelisted create fields

--- index.ts
+++ index.ts
@@ -8,13 +8,43 @@ export { apiConfig } from "./endpoints";
 export const migrations = [init, addTagType, updateContentType];
 export const setupMigrations = [init, initSettings];
 
-FieldRegistry.register("settings", ["key", "value"]);
-FieldRegistry.register("tagGroups", ["name", "filterable", "visible", "order", "type"]);
-FieldRegistry.register("tags", ["name", "filterable", "order"]);
-FieldRegistry.register("synonyms", ["canonical", "synonym"]);
-FieldRegistry.register("banners", ["tag", "name", "title", "description", "url", "link", "activeFrom", "activeTo", "order", "buttonText", "buttonLink", "buttonLocation", "buttonTextAlt", "buttonLinkAlt", "buttonLocationAlt"]);
-FieldRegistry.register("linkLists", ["name", "key"]);
-FieldRegistry.register("links", ["text", "url", "subMenuKey", "order"]);
-FieldRegistry.register("content", ["slug", "title", "type", "content", "layout", "format", "enabled", "publishDate"]);
-FieldRegistry.register("media", ["url", "altText", "title", "caption"]);
-FieldRegistry.register("plugins", ["group", "key", "sortOrder", "settings"]);
+FieldRegistry.register("settings", {
+    create: ["key", "value"],
+    update: ["key", "value"]
+});
+FieldRegistry.register("tagGroups", {
+    create: ["name", "filterable", "visible", "order", "type"],
+    update: ["name", "filterable", "visible", "order", "type"]
+});
+FieldRegistry.register("tags", {
+    create: ["name", "groupId", "filterable", "order"],
+    update: ["name", "filterable", "order"]
+});
+FieldRegistry.register("synonyms", {
+    create: ["canonical", "synonym"],
+    update: ["canonical", "synonym"]
+});
+FieldRegistry.register("banners", {
+    create: ["tag", "name", "title", "description", "url", "link", "activeFrom", "activeTo", "order", "buttonText", "buttonLink", "buttonLocation", "buttonTextAlt", "buttonLinkAlt", "buttonLocationAlt"],
+    update: ["tag", "name", "title", "description", "url", "link", "activeFrom", "activeTo", "order", "buttonText", "buttonLink", "buttonLocation", "buttonTextAlt", "buttonLinkAlt", "buttonLocationAlt"]
+});
+FieldRegistry.register("linkLists", {
+    create: ["name", "key"],
+    update: ["name", "key"]
+});
+FieldRegistry.register("links", {
+    create: ["listId", "text", "url", "subMenuKey", "order"],
+    update: ["text", "url", "subMenuKey", "order"]
+});
+FieldRegistry.register("content", {
+    create: ["slug", "title", "type", "content", "layout", "format", "enabled", "publishDate"],
+    update: ["slug", "title", "type", "content", "layout", "format", "enabled", "publishDate"]
+});
+FieldRegistry.register("media", {
+    create: ["url", "altText", "title", "caption"],
+    update: ["url", "altText", "title", "caption"]
+});
+FieldRegistry.register("plugins", {
+    create: ["group", "key", "sortOrder", "settings"],
+    update: ["group", "key", "sortOrder", "settings"]
+});
